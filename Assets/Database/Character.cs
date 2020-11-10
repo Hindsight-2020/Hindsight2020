2b11f456b8db18b374c0034e78d6e0d42d4fabfc
jaberrio <jaberrio88@hotmail.com>
2020-11-10 17:53:14 -0500

    Database Phase One

---
+++ Assets/Database/Character.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public string name;
+    public float hat_R;
+    public float hat_G;
+    public float hat_B;
+
+
+}
---
+++ Assets/Database/CharacterData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CharacterData
+{
+    public string name;
+    public float hat_R;
+    public float hat_G;
+    public float hat_B;
+
+    public CharacterData(Character character)
+    {
+        name = character.name;
+        hat_R = character.hat_R;
+        hat_G = character.hat_G;
+        hat_B = character.hat_B;
+    }
+}
