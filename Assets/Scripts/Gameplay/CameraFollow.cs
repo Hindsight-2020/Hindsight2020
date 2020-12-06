5238b73b0e48f988ed55e2605619bf34e77f878d
rickrekkem <58759497+rickrekkem@users.noreply.github.com>
2020-12-06 16:05:10 -0500

    Merged
    Merged local changes to other files

---
+++ Assets/Scripts/Gameplay/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform followTransform;
+
+    void FixedUpdate()
+    {
+        this.transform.position =
+            new Vector3(followTransform.position.x + 0.5f, followTransform.position.y - 0.5f, this.transform.position.z);
+    }
+}
